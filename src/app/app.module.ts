2866b267a91187952aca79804c506d6793c17ca3
bluemax <maxi@schremser.com>
2017-03-04 19:14:33 +0100

    add bootstrap and ibm-design-colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule }            from '@ng-bootstrap/ng-bootstrap';
+import { NgbdTabsetBasic } from './tabset-basic';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdTabsetBasic
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
