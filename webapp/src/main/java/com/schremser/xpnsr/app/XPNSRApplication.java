dc8c1c03d4cc6182ac2f915050e62de6001e8f8d
bluemax <maxi@schremser.com>
2017-05-01 10:47:06 +0200

    update node packages

--- webapp/src/main/java/com/schremser/xpnsr/app/XPNSRApplication.java
+++ webapp/src/main/java/com/schremser/xpnsr/app/XPNSRApplication.java
@@ -10,21 +10,6 @@ import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
-/*
- * Notes:
- * 
- * - injection of ServletContext did not work. Using ServletContextListener instead
- * 
- * - 'mock' mode
- * If ALGO_HOME or ALGO_TOP are not defined launching in 'mock' mode
- * This means basically that mock implementations of LoginService and others may be launched
- * In 'mock' mode needs to assure that no calls to configuration and logging are made as this will 
- * cause attempts by those modules to contact the naming service
- * 
- * 
- */
-
-
 @ApplicationPath( "/api" )
 public class XPNSRApplication extends Application {
 	private final static Logger log = LoggerFactory.getLogger(XPNSRApplication.class);
@@ -43,7 +28,6 @@ public class XPNSRApplication extends Application {
 		log.debug( "XPNSRApplication: getSingletons" );
 
 		// Initialize ProviderRegistry
-		//
 		ProviderRegistry.instance( new SimpleProviderFactory() );
 
 		Set<Object> singletons = new HashSet<>( );
@@ -51,7 +35,6 @@ public class XPNSRApplication extends Application {
 		singletons.add(new ExpenseResource());
 		singletons.add(new CORSFilter());
 
-		// singletons.add( new TestRequestFilter( ) );
 		log.info( "Number of singletons: " + singletons.size( ) );
 		
 		return singletons;
