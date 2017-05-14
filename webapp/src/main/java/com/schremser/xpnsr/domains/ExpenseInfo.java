5256671a2cb0b766ea60c8a417531b76bcf98a46
bluemax <maxi@schremser.com>
2017-05-14 20:49:07 +0200

    use ibm design colors

--- webapp/src/main/java/com/schremser/xpnsr/domains/ExpenseInfo.java
+++ webapp/src/main/java/com/schremser/xpnsr/domains/ExpenseInfo.java
@@ -2,7 +2,9 @@ package com.schremser.xpnsr.domains;
 
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Date;
+import java.util.Locale;
 
 /*
  * ExpenseInfo
@@ -20,56 +22,60 @@ import java.util.Date;
 
  */
 public class ExpenseInfo extends ResourceItemBase {
-	private ExpenseType i_type;
-	private Double d_amount;
-	private Date i_date; // YYYYMMDD format
-	private String i_description;
+    private ExpenseType i_type;
+    private Double d_amount;
+    private Date i_date; // YYYYMMDD format
+    private String i_description;
 
     private final String DECIMAL_PATTERN = "#0.00";
     private final DecimalFormat df = new DecimalFormat(DECIMAL_PATTERN);
 
     public ExpenseInfo() {
-	}
-	
-	public ExpenseInfo(ExpenseInfo source ) {
-		i_type = source.i_type;
-		i_date = source.i_date;
-		i_description = source.i_description;
-	}
-	
-	public void setType( ExpenseType type ) {
-		i_type = type;
-	}
-	
-	public ExpenseType getType( ) {
-		return i_type;
-	}
-	
-	public Date getDate( ) {
-		return i_date;
-	}
-	
-	public void setDate( Date expenseDate ) {
-		i_date = expenseDate;
-	}
-	
-	public void setDescription( String description ) {
-		i_description = description;
-	}
-	
-	public String getDescription( ) {
-		return i_description;
-	}
+        DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.getDefault());
+        dfs.setDecimalSeparator('.');
+        df.setDecimalFormatSymbols(dfs);
+    }
 
-	public Double getAmount() {
-		return d_amount;
-	}
+    public ExpenseInfo(ExpenseInfo source) {
+        super();
+        i_type = source.i_type;
+        i_date = source.i_date;
+        i_description = source.i_description;
+    }
 
-	public void setAmount(Double amount) {
-		this.d_amount = Double.valueOf(df.format(amount));
-	}
+    public void setType(ExpenseType type) {
+        i_type = type;
+    }
 
-	public void setAmount(Integer amount) {
-		this.d_amount = Double.valueOf(amount);
-	}
+    public ExpenseType getType() {
+        return i_type;
+    }
+
+    public Date getDate() {
+        return i_date;
+    }
+
+    public void setDate(Date expenseDate) {
+        i_date = expenseDate;
+    }
+
+    public void setDescription(String description) {
+        i_description = description;
+    }
+
+    public String getDescription() {
+        return i_description;
+    }
+
+    public Double getAmount() {
+        return d_amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.d_amount = Double.valueOf(df.format(amount));
+    }
+
+    public void setAmount(Integer amount) {
+        this.d_amount = Double.valueOf(amount);
+    }
 }
